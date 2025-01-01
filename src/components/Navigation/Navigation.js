cd7496e46d2bbefb6b0fb6dd1a8ad09a1a10bba9
NaikChetana <chetananaik73@gmail.com>
2025-01-01 13:14:41 +0100

    chore(CI/CD): test build and deploy workflow files

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Navigation = ()=>{
   return (
     <nav style={{display: 'flex', justifyContent:"flex-end"}}>
-      <p className="f3 link dim black underline p3 pointer">Sign out</p>
+      <p className="f3 link dim black underline p3 pointer">Sign_out_btn</p>
     </nav>
   )
 }
